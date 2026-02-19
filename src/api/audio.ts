7417e363bb35a4e80cc9bda2e89d9002492e6979
Anton Bilbaeno <antoaenono@pm.me>
2026-02-18 21:52:46 -0800

    refactor: hard-code -> find first mp3

--- src/api/audio.ts
+++ src/api/audio.ts
@@ -2,16 +2,18 @@
  * @module api/audio
  */
 import { Router } from 'express'
-import path from 'path'
+
+import { firstMp3 } from '../media'
 
 export const audioRouter = Router()
 
-audioRouter.get('/audio', (req, res, next) => {
-  const filePath = path.join(
-    import.meta.dirname,
-    '../../media/Lobo Loco - After Midnight Walk (ID 2412).mp3',
-  )
-  res.sendFile(filePath, (err) => {
-    if (err) next(err)
-  })
+audioRouter.get('/audio', async (req, res, next) => {
+  try {
+    const filePath = await firstMp3()
+    res.sendFile(filePath, (err) => {
+      if (err) next(err)
+    })
+  } catch (err) {
+    next(err)
+  }
 })
